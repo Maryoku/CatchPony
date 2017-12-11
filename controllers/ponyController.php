710e677d02ea01e672a39990ab6ee58c863762cb
Maryoku <maryoku.amaya@gmail.com>
2017-12-12 04:14:39 +0500

    add web-game solution

---
+++ controllers/ponyController.php
@@ -0,0 +1,60 @@
+<?php
+
+class ponyController extends Controller
+{
+    public function index()
+    {
+        $ponies = $this->model->load();
+        $this->setResponse($ponies);
+    }
+
+    public function view($data)
+    {
+        $pony = $this->model->load($data['id']);
+        $this->setResponse($pony);
+    }
+
+    public function add()
+    {
+        $postData=json_decode(file_get_contents('php://input'), TRUE);
+
+        if(isset($postData['id']) && isset($postData['name']) && isset($postData['image'])
+            && isset($postData['power']) && isset($postData['speed'])){
+
+            $dataToSave = array(
+                'id' => $postData['id'],
+                'name' => $postData['name'],
+                'image' => $postData['image'],
+                'power' => $postData['power'],
+                'speed' => $postData['speed']);
+
+            $addedItem=$this->model->create($dataToSave);
+            $this->setResponse($addedItem);
+        }
+    }
+
+    public function edit($data)
+    {
+        $postData=json_decode(file_get_contents('php://input'), TRUE);
+
+        if(isset($postData['id']) && isset($postData['name']) && isset($postData['image'])
+            && isset($postData['power']) && isset($postData['speed'])){
+
+            $dataToSave = array(
+                'id' => $postData['id'],
+                'name' => $postData['name'],
+                'image' => $postData['image'],
+                'power' => $postData['power'],
+                'speed' => $postData['speed']);
+
+            $updateItem = $this->model->save($data['id'], $dataToSave);
+            $this->setResponse($updateItem);
+        }
+    }
+
+    public function delete($data)
+    {
+        $deleteItem = $this->model->delete($data['id']);
+        $this->setResponse($deleteItem);
+    }
+}
